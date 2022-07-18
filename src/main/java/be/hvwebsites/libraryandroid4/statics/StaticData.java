289e7621ca46f5b78e636c4d4f4bcc4d32fb178a
jm1 <jm1441@proximus.be>
2022-07-18 13:57:19 +0200

    Versie 1 trachten winkelen app in zijn laatste versie te restoren

--- src/main/java/be/hvwebsites/libraryandroid4/statics/StaticData.java
+++ src/main/java/be/hvwebsites/libraryandroid4/statics/StaticData.java
@@ -34,4 +34,23 @@ public class StaticData {
     public static final String PURPLE_500 = "purple_500";
     public static final String BOLD_TEXT = "bold";
 
+    // Bluetooth communication statics
+    // Message types sent from the BluetoothChatService Handler
+    public static final int MESSAGE_STATE_CHANGE = 1;
+    public static final int MESSAGE_READ = 2;
+    public static final int MESSAGE_WRITE = 3;
+    public static final int MESSAGE_DEVICE_NAME = 4;
+    public static final int MESSAGE_TOAST = 5;
+    public static final int MESSAGE_CONVERSATION_AREA = 6;
+    public static final String CONVERSATION_MSG = "Bt msg";
+
+    // Bluetooth returncodes
+    public static final int BLUETOOTH_NOT_SUPPORTED = 888;
+    public static final int BLUETOOTH_ENABLE_FAILED = 887;
+
+    // Key names received from the BluetoothChatService Handler
+    public static final String DEVICE_NAME = "device_name";
+    public static final String TOAST = "toast";
+
+
 }
